637968fd3dcdf1474164365c4b9100d55c2d4932
imFishray <yulielove0530@gmail.com>
2022-11-30 03:49:50 +0800

    調整側邊攔

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
-  <Aside></Aside>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/news">最新消息管理</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Aside/>
 </template>
 
 <style lang="scss">
-
+@import "../src/assets/Css/Reset.css";
+@import "../src/assets/Scss/base/font.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
 
--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -9,6 +9,7 @@
         <li><a href="#">最新消息管理</a></li>
         <li><a href="#">行程方案管理</a></li>
         <li><a href="#">行程訂單管理</a></li>
+        <li><a href="#">開團管理</a></li>
         <li><a href="#">商品管理</a></li>
         <li><a href="#">商品訂單管理</a></li>
         <li><a href="#">意見表管理</a></li>
@@ -28,35 +29,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import "../assets/Css/Reset.css";
+@import "../assets/Scss/base/font.scss";
 .nav-aside{
-  width: 18%;
-  height: 90vh; 
+  position: fixed;
+  width: 300px;
+  height: 100vh; 
   background-color: #2D3740;
-  
+  .nav-aside-logo img{
+    margin: 10px 60px;
+    width: 150px;
+  }
+  ul{
+    text-align: start;
+    li{
+      margin: 30px 75px;
+      color: #fff;
+      font-size: nth($font-size, 6)+ px;
+      /* 如何消除marker? */
+      a{
+        direction: none;
+        color: #fff;
+      }
+    }
+  }
 }
-
-.nav-aside-logo img{
-  margin: 10px 0;
-  width: 150px;
-}
-
-.nav-aside ul{
-  
-}
-
-.nav-aside li{
-  margin: 20px 0;
-  color: #fff;
-  text-align: start;
-  /* 如何消除marker? */
-  margin-left: 8px;    
-}
-
-.nav-aside a{
-  direction: none;
-  color: #fff;
-
-}
-
 </style>
