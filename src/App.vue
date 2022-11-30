9daecad3b2c720bfe1624ca334f2874006cea575
chubbyrabi <chubbytobey@gmail.com>
2022-11-30 13:15:03 +0800

    fix 側邊欄

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <Aside/>
+  <div class="container">
+    <Aside/>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
 @import "../src/assets/Css/Reset.css";
+@import "../src/assets/Scss/base/common.scss";
 @import "../src/assets/Scss/base/font.scss";
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
+
 </style>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
+  <div class="home main">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
   </div>
-
-  <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
 </template>
 
 <script>
