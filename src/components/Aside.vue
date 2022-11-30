9fac937cb3fa41a934790a7681cc583f47245fc5
chubbyrabi <chubbytobey@gmail.com>
2022-12-01 00:20:42 +0800

    add QA

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="nav-aside">
-        <div class="nav-aside-logo">
-            <img src="../assets/logo_WHITE.png" alt="logo">
-        </div>
-        <hr>
-        <nav>
-          <!-- <router-link to="/">初始歡迎頁</router-link> -->
-          <router-link to="/">帳號管理</router-link>
-          <router-link to="/news">最新消息管理</router-link>
-          <router-link to="/">行程方案管理</router-link>
-          <router-link to="/">行程訂單管理</router-link>
-          <router-link to="/">開團管理</router-link>
-          <router-link to="/">商品管理</router-link>
-          <router-link to="/">商品訂單管理</router-link>
-          <router-link to="/">意見表管理</router-link>
-          <router-link to="/">Q&A 管理</router-link>
-        </nav>
-  </div>
+	<div class="nav-aside">
+		<div class="nav-aside-logo">
+			<img src="../assets/logo_WHITE.png" alt="logo">
+		</div>
+		<hr>
+		<nav>
+			<!-- <router-link to="/">初始歡迎頁</router-link> -->
+			<router-link to="/">帳號管理</router-link>
+			<router-link to="/news">最新消息管理</router-link>
+			<router-link to="/">行程方案管理</router-link>
+			<router-link to="/">行程訂單管理</router-link>
+			<router-link to="/">開團管理</router-link>
+			<router-link to="/">商品管理</router-link>
+			<router-link to="/">商品訂單管理</router-link>
+			<router-link to="/">意見表管理</router-link>
+			<router-link to="/QA">Q&A 管理</router-link>
+		</nav>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Aside',
-  props: {
-    msg: String
-  }
+	name: 'Aside',
+	props: {
+		msg: String
+	}
 }
 </script>
 
@@ -33,26 +33,25 @@ export default {
 @import "../assets/Css/Reset.css";
 @import "../assets/Scss/base/font.scss";
 .nav-aside{
-  position: fixed;
-  width: 300px;
-  height: 100vh; 
-  background-color: #2D3740;
-  .nav-aside-logo img{
-    margin: 10px 60px;
-    width: 150px;
-  }
+	position: fixed;
+	width: 300px;
+	height: 100vh; 
+	background-color: #2D3740;
+	.nav-aside-logo img{
+		margin: 10px 60px;
+		width: 150px;
+	}
 }
-  nav{
-    text-align: start;
-  }
-
-  nav a{
-    display: block;
-    margin: 30px 75px;
-    font-size: nth($font-size, 6)+ px;
-    direction: none;
-    color: #fff;
-  }
+nav{
+	text-align: start;
+}
+nav a{
+	display: block;
+	margin: 30px 75px;
+	font-size: nth($font-size, 6)+ px;
+	direction: none;
+	color: #fff;
+}
 
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,34 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/news',
-    name: 'news',
-    component: () => import('../views/NewsView.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+{
+	path: '/',
+	name: 'home',
+	component: HomeView
+},
+{
+	path: '/news',
+	name: 'news',
+	component: () => import('../views/NewsView.vue')
+},
+{
+	path: '/QA',
+	name: 'QA',
+	component: () => import('../views/QAView.vue')
+},
+{
+	path: '/about',
+	name: 'about',
+	// route level code-splitting
+	// this generates a separate chunk (about.[hash].js) for this route
+	// which is lazy-loaded when the route is visited.
+	component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+}
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
