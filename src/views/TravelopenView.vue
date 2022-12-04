69f90622597d30f253ba7edd5274e8ee9cfe1174
tangtang1b1b <a0981632653@gmail.com>
2022-12-05 00:39:22 +0800

    歐噴醬

--- src/views/TravelopenView.vue
+++ src/views/TravelopenView.vue
@@ -1,9 +1,8 @@
 <template>
-    <!-- style="display:none" -->
     <div class="backstage-news" >
         <div class="backstage-name">
             <div>
-                <h2 class="font-36">行程方案管理</h2>
+                <h2 class="font-36">開團管理</h2>
             </div>
             <div class="backstage-account">
                 <span class="font-18">管理員名稱</span>
@@ -11,7 +10,62 @@
             </div>
         </div>
         <div class="backstage-content">
-            <div class="backstage-path font-16">行程方案管理 / 行程方案列表</div>
+            <div class="backstage-path font-16">開團管理 / 開團設定</div>
+        </div>
+        <div class="container">
+            <div class="content">
+                <div class="type opencode">
+                    <p class="nname">開團編號</p>
+                    <p class="succe">A0001</p>
+                </div>
+                <div class="type travelcode">
+                    <p class="nname">行程方案</p>
+                    <div class="choose">
+                        <select class="choosein" v-model="selected">
+                            <option value="" disabled>-請選擇-</option>
+                            <option>方案A</option>
+                            <option>方案B</option>
+                            <option>方案C</option>
+                            <option>方案D</option>
+                        </select>
+                    </div>
+                </div>
+                
+                <div class="type travelcode">
+                    <p class="nname">出發日期</p>
+                    <Space size="large" wrap>
+                        <DatePicker type="date" placeholder="Select date" style="width: 300px" />
+                    </Space>
+                </div>
+                
+                <div class="type travelcode">
+                    <p class="nname">開放報名日期</p>
+                    <Space size="large" wrap>
+                        <DatePicker type="date" placeholder="Select date" style="width: 300px" />
+                    </Space>
+                </div>
+                
+                <div class="type travelcode">
+                    <p class="nname">截止日期</p>
+                    <Space size="large" wrap>
+                        <DatePicker type="date" placeholder="Select date" style="width: 300px" />
+                    </Space>
+                </div>
+                
+                <div class="type travelcode">
+                    <p class="nname">人數上限</p>
+                    <Input class="succe" type="text" v-model="value" style="width: 300px" />
+                </div>
+                
+                <div class="type travelcode">
+                    <p class="nname">價格 / 人</p>
+                    <Input class="succe" type="text" v-model="value" style="width: 300px" />
+                </div>
+                <div class="delcan">
+                    <div class="shure">取消</div>
+                    <div class="shure">確認</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,248 +73,20 @@
     export default {
         data () {
             return {
-                columns: [
-                    
-            {
-                title: '前台區塊',
-                slot: 'news_session',
-                width: 100,
-                align: 'center'
-            },
-            {
-                title: '方案編號',
-                slot: 'news_no',
-                width: 100,
-                align: 'center'
-            },
-            {
-                title: '上架日期',
-                slot: 'news_time',
-                width: 110,
-                align: 'center'
-            },
-            {
-                title: '標題',
-                slot: 'news_title',
-                align: 'center'
-            },
-            {
-                title: '推薦商品',
-                slot: 'news_class',
-                width: 150,
-                align: 'center'
-            },
-            {
-                title: '操作',
-                slot: 'action',
-                width: 100,
-                align: 'center'
-            },
-            {
-                title: '狀態',
-                slot: 'news_status',
-                width: 70,
-                align: 'center'
-            }
-                ],
-                dataOn: [
-                    {
-                        news_no: 'A1100001',
-                        news_time: '2022/11/22',
-                        news_session:'A',
-                        news_class: '火車佐便當',
-                        news_title: '「高千穗峽谷」划船體驗報名優惠',
-                        news_status:'上架',
-                    },
-                    {
-                        news_no: 'A1100002',
-                        news_time: '2022/11/23',
-                        news_session:'B',
-                        news_class: '火車佐便當',
-                        news_title: '高千穗-夜神樂',
-                        news_status:'上架',
-                    },
-                    {
-                        news_no: 'A1100003',
-                        news_time: '2022/11/24',
-                        news_session:'C',
-                        news_class: '火車佐便當',
-                        news_title: '「高千穗峽谷」划船體驗報名優惠',
-                        news_status:'上架',
-                    },
-                    {
-                        news_no: 'A1100004',
-                        news_time: '2022/11/25',
-                        news_session:'D',
-                        news_class: '火車佐便當',
-                        news_title: '「鹿兒島沙浴」體驗活動報名',
-                        news_status:'上架',
-                    },
-                ],
-                dataDraft: [
-                    {
-                        news_no: 'A1100005',
-                        news_time: '2022/11/23',
-                        news_session:'E',
-                        news_class: '火車佐便當',
-                        news_title: '高千穗-夜神樂',
-                        news_status:'草稿',
-                    },
-                    {
-                        news_no: 'A1100006',
-                        news_time: '2022/11/24',
-                        news_session:'F',
-                        news_class: '火車佐便當',
-                        news_title: '列車停駛',
-                        news_status:'草稿',
-                    }
-                ],
-                dataOff: [
-                    {
-                        news_no: 'A1100007',
-                        news_time: '2022/11/25',
-                        news_session:'G',
-                        news_class: '火車佐便當',
-                        news_title: '「鹿兒島沙浴」體驗活動報名',
-                        news_status:'下架',
-                    },
-                    {
-                        news_no: 'A1100008',
-                        news_time: '2022/11/26',
-                        news_session:'H',
-                        news_class: '火車佐便當',
-                        news_title: '商城新品上市!!',
-                        news_status:'下架',
-                    }
-                ],
-                editIndex: -1,  // 当前聚焦的输入框的行数
-                editnews_no: '',  // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
-                editnews_time: '',  // 第二列输入框
-                editnews_class: '',  // 第三列输入框
-                editnews_title: '',  // 第四列输入框
-                editnews_status: '',
-                editnews_session: '',
-                }
-            },
-            methods: {
-            handleEdit (row, index) {
-                this.editnews_no = row.news_no;
-                this.editnews_time = row.news_time;
-                this.editnews_title = row.news_title;
-                this.editnews_class = row.news_class;
-                this.editnews_status = row.news_status;
-                this.editnews_session = row.news_session;
-                this.editIndex = index;
-            },
-            handleSave (index) {
-                this.data[index].news_no = this.editnews_no;
-                this.data[index].news_time = this.editnews_time;
-                this.data[index].news_class = this.editnews_class;
-                this.data[index].news_title = this.editnews_title;
-                this.data[index].news_status = this.editnews_status;
-                this.data[index].news_session = this.editnews_session;
-                this.editIndex = -1;
+                selected:"",
+                value:"",
             }
         }
-
     }
 </script>
 
 <style scoped lang="scss">
-/* -------------------彈窗 ------------------------*/
-.popup{
-    position: absolute;
-    top: 5px;
-    left: 150px;
-    margin: auto;
-    z-index: 10;
+@import "@/assets/Scss/base/color.scss";
 
-    width: 70%;
-    background-color: #4F6573;
-}
+// *{
+//     outline: solid 1px;
+// }
 
-/* 上方區塊顏色 */
-.popup-head{
-    background-color: #2D3740;
-    height: 80px;
-    color: #ccc;
-
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-}
-
-.popup-data{
-    margin: 30px 0;
-    display: flex;
-    justify-content: space-around;
-}
-.input-title{
-    margin-bottom: 40px;
-    padding-left: 85px;
-}
-.input-des,
-.input-pic,
-.input-pic-des{
-    margin-bottom: 15px;
-    padding-left: 85px;
-
-}
-.input-txt{
-    margin-bottom: 40px;
-}
-.popup-btn{
-    margin: 90px 0 50px 0 ;
-    display: flex;
-    justify-content: center;
-
-}
-
-.popup-btn button{
-    margin: 0 100px;
-}
-
-/* 彈窗字顏色 */
-.popup label{
-    color: #fff;
-}
-.date{
-    color: #ccc;
-}
-
-
-/* -------------------彈窗結束----------------- */
-
-/* -------------------新增修改box ------------------------*/
-.popup-box{
-    position: absolute;
-    top: 200px;
-    left: 400px;
-    width: 400px;
-    height: 230px;
-    border: 1px solid #6c9255;
-    border-radius: 10px;
-    background-color: #ffffff;
-    text-align: center;
-}   
-
-.popup-box-close{
-    text-align: end;
-    margin-top: 10px;
-    padding-right: 20px;
-}
-
-.check-des{
-    margin-top: 70px;
-}
-
-.popup-box button{
-    margin-top: 30px;
-}
-
-/* -------------------新增修改結束 ------------------------*/
-
-/* 後台header */
 .backstage-name{
     display: flex;
     justify-content: space-between;
@@ -273,40 +99,63 @@
 .backstage-name h2{
     margin-left: 40px;
 }
-/* 帳號狀態 */
 .backstage-account span{
     display: inline-block;
     cursor: pointer;
     margin: 0 20px;
 }
-
-/* 消息管理 */
-/* 麵包屑 */
 .backstage-path{
     text-align: start;
     color: #888888;
-    margin: 40px 0 0 40px;
+    margin: 40px 0 0px 40px;
+}
+.container{
+    padding: 40px 0px;
+    width: 100%;
+    .content{
+        width: 100%;
+        max-width: 768px;
+        margin: auto;
+        background-color: $back_color_active;
+        .delcan{
+            width: 100%;
+            max-width: 30%;
+            display: flex;
+            justify-content: space-between;
+            margin: 20px auto;
+            .shure{
+                background-color: $back_color_main;
+                padding: 5px 30px;
+                border-radius: 10px;
+                color: #fff;
+                cursor: pointer;
+            }
+            .shure:nth-child(1){
+                background-color: #fff;
+                color: $back_color_main;
+            }
+        }
+        .type{
+            display: flex;
+            color: #fff;
+            .nname{
+                width: 50%;
+                padding: 30px 0px;
+            }
+            .choose{
+                height: 40px;
+                display: flex;
+                margin: auto auto auto 0px;
+                .choosein{
+                    width: 300px;
+                }
+            }
+            .succe{
+                display: flex;
+                align-items: center;
+            }
+        }
+    }
 }
 
-/* 新增按鈕 */
-.btn-add{
-    text-align: end;
-    margin-right: 60px;
-}
-/* 管理介面 */
-.news-manager{
-    height: 45vh;
-    margin: 30px 20px;
-}
-
-/* 上下頁 */
-.btn-bottom{
-    text-align: end;
-    margin:100px 60px 0 0 ;
-    margin-right: 60px;
-}
-
-.btn-bottom button{
-    margin-left: 80px;
-}
 </style>
