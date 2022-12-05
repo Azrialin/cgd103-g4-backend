df10f59b9aa21c1b50175288ae09aa96f1c995ab
Yun097 <cream86099797@gmail.com>
2022-12-06 01:12:48 +0800

    新增帳號彈窗

--- src/views/AdministratorView.vue
+++ src/views/AdministratorView.vue
@@ -12,7 +12,8 @@
         <div class="backstage-content">
             <div class="backstage-path font-16">帳號管理 / 管理者帳號</div>
             <div class="btn-add">
-                <Button type="primary">新增帳號</Button>
+                <Button type="primary"
+                @click="addToggle">新增帳號</Button>
             </div>
         </div>
         <div class="administrator-manager">
@@ -22,7 +23,7 @@
                     <Input type="text" v-if="editIndex === index" />
                         <span v-else>{{ row.staff_no }}</span>
                 </template>
-                <!-- 帳號 -->
+                <!-- 員工帳號 -->
                 <template #staff_number="{ row, index }">
                     <Input type="text" v-if="editIndex === index" />
                         <span v-else>{{ row.staff_number }}</span>
@@ -43,12 +44,56 @@
                 </template>
                 <!-- 刪除帳號 -->
                 <template #action_error="{ row, index }">
-                    <Button type="error" size="small" @click="remove(index)">刪除</Button>
+                    <Button type="error" size="small" @click="remove(index, row)">刪除</Button>
                 </template>
             </Table>
         </div>
     </div>
     <Page :total="40" size="small" />
+
+    <!-- style="display:none" -->
+    <!--一張全新表單 -->
+    <div class="popup " v-show="seenAdd" >
+        <div class="popup-head font-20">新增員工帳號</div>
+        <div class="popup-content font-18">
+            <div class="input-txt">
+                <div class="input-info">
+                    <label for="">員工編號：
+                        <Input type="text" placeholder="" clearable style="width: 200px" />
+                    </label>
+                </div>
+                <div class="input-info">
+                    <label for="">員工帳號：
+                        <Input type="text" placeholder="英數10個字" clearable  style="width: 200px"/>
+                    </label>
+                </div>
+                <div class="input-info">
+                    <label for="">員工密碼：
+                        <Input type="password" placeholder="英數8個字" clearable  style="width: 200px"/>
+                    </label>
+                </div>
+                <div class="input-info">
+                    <label for="">員工姓名：
+                        <Input type="text" placeholder="請輸入姓名" clearable  style="width: 200px"/>
+                    </label>
+                </div>
+                <div class="input-info">
+                    <label for="">員工信箱：
+                        <Input type="email" placeholder="請輸入Email" clearable  style="width: 200px"/>
+                    </label>
+                </div>
+            </div>
+            <div class="input-switchs">
+                <label class="state" for="">帳號狀態：
+                    <Switch true-color="#13ce66" false-color="#E6E6E6" />
+                </label>
+            </div>
+            <div class="popup-btn">
+                <Button type="primary">新增帳號</Button>
+                <Button @click="addToggle">取消</Button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -59,6 +104,8 @@ export default {
 	},
     data () {
         return {
+            seenAdd:false, //新表格彈窗，綁新表單v-show、按鈕@click="seenAdd"
+            size:'default', //按鈕間距，搭配Space，預設small(無間距)， 可自行調整距離px，詳情請看 https://run.iviewui.com/
             columns: [
                 {
                     title: '員工編號',
@@ -66,7 +113,7 @@ export default {
                     align: 'center'
                 },
                 {
-                    title: '帳號',
+                    title: '員工帳號',
                     slot: 'staff_number',
                     align: 'center'
                 },
@@ -130,12 +177,16 @@ export default {
         },
         remove (index) {
             this.data.splice(index, 1);
+        },
+        addToggle(){ //新表單
+            this.seenAdd = !this.seenAdd
         }
     }
 }
 </script>
 
 <style scoped lang="scss">
+
 /* 後台header */
 .backstage-name{
     display: flex;
@@ -168,7 +219,6 @@ export default {
 .btn-add{
     text-align: end;
 }
-
 .ivu-btn-primary{
     text-align: end;
     margin-right: 40px;
@@ -176,10 +226,9 @@ export default {
     border: #0C3B59;
     color: #fff;
 }
-
 .ivu-btn-primary:hover{
+    // background-color: #4F6573;
     background-color: #A3AFBF;
-    border: #A3AFBF;
 }
 
 /* 管理介面 */
@@ -187,4 +236,61 @@ export default {
     height: 45vh;
     margin: 30px 40px;
 }
+
+/* -------------------彈窗 ------------------------*/
+.popup{
+    position: absolute;
+    top: 60px;
+    left: 300px;
+    margin: auto;
+    z-index: 10;
+
+    width: 40%;
+    background-color: #4F6573;
+}
+/* 上方區塊顏色 */
+.popup-head{
+    background-color: #2D3740;
+    height: 40px;
+    color: #ccc;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.input-txt{
+    margin-top: 50px;
+}
+.input-info{
+    margin-bottom: 40px;
+    padding-left: 85px;
+}
+.input-switchs{
+    margin-bottom: 15px;
+    padding-left: 85px;
+}
+.input-txt{
+    margin-bottom: 40px;
+}
+.popup-btn{
+    margin: 70px 90px 50px 0 ;
+    display: flex;
+    justify-content: right;
+
+}
+
+.popup-btn Button{
+    margin: 0 20px;
+}
+
+/* 彈窗字顏色 */
+.popup label{
+    color: #fff;
+}
+.date{
+    color: #ccc;
+}
+
+
+/* -------------------彈窗結束----------------- */
 </style>
