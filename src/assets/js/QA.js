a4855c3385cac4213654d71040b9b83bed845787
chubbyrabi <chubbytobey@gmail.com>
2022-12-06 03:13:29 +0800

    改個 v-for

--- src/assets/js/QA.js
+++ src/assets/js/QA.js
@@ -1,12 +1,10 @@
-// click
 $(function menuClick(){
-    $('.parent-list>li>a').on('click', function (){
-        $('.parent-list>li>a').not(this).removeClass('on');
-        $('.parent-list>li>a').not(this).removeClass('show');
-        $(this).addClass('on');
+    $('.parent-list>li').on('click', function (){
+        $('.parent-list>li').not(this).find('a').removeClass('show');
     });
+    
     $('.child-list li a').on('click', function (){
-        $('.child-list li a').not(this).removeClass('on');
-        $(this).addClass('on');
+        $('.child-list li a').not(this).removeClass('show');
+        $(this).addClass('show');
     });
 });
