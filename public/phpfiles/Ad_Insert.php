6c8bfc80501f9a4357ed60e9db317c97d99da7f8
Azrialin <zxe2124@gmail.com>
2022-12-27 16:51:03 +0800

    adinsert updated

--- public/phpfiles/Ad_Insert.php
+++ public/phpfiles/Ad_Insert.php
@@ -7,12 +7,12 @@ try{
 	require_once("./connect_cgd103g4.php");
 	// sql 指令
 	// 跟資料庫表格的欄位數目必須相同
-	$id = $_POST['emp_id'];
-	$sql = "select * from `emp` where emp_id = :emp_id";
-	$sql = "insert into emp values (null, :emp_id, :emp_psw, :emp_name, :emp_email, :emp_status)";
+	//$id = $_POST['emp_id'];
+	//$sql = "select * from `emp` where emp_id = :emp_id";
+	$sql = "insert into emp values (:emp_psw, :emp_name, :emp_email, :emp_status)";
 	// 編譯, 執行
 	$faq = $pdo->prepare($sql);
-	$faq->bindValue(":emp_id", $_POST["emp_id"]);
+	// $faq->bindValue(":emp_id", $_POST["emp_id"]);
 	$faq->bindValue(":emp_psw", $_POST["emp_psw"]);
 	$faq->bindValue(":emp_name", $_POST["emp_name"]);
 	$faq->bindValue(":emp_email", $_POST["emp_email"]);
